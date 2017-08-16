9a10506b591e12bd8fea93abd1ed43a16a127dfc
yvasilyev92 <yvasilye@mail.sfsu.edu>
2017-08-16 15:58:06 -0700

    Added IdlingResourceMenuActivityTest

---
+++ app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
@@ -0,0 +1,86 @@
+package com.example.android.teatime;
+
+/**
+ * Created by Yevgeniy on 8/16/2017.
+ */
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import static org.hamcrest.Matchers.anything;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Usually Espresso syncs all view operations with the UI thread as well as AsyncTasks, but it can't
+ * do so with custom resources (e.g. activity or service). For such cases, we can register the
+ * custom resource and Espresso will wait for the resource to be idle before
+ * executing a view operation.
+ *
+ * In this example, we simulate an idling situation. This test is the same as the
+ * MenuActivityScreenTest but with an Idling Resource to help with synchronization.
+ *
+ * We added an idling period from when the user clicks on a GridView item
+ * in MenuActivity to when corresponding order activity appears. This is to simulate potential
+ * delay that could happen if this data were being retrieved from the web. Without registering the
+ * custom resources, this test would fail because the test would proceed without waiting
+ * for the Idling Resource.
+ */
+
+
+@RunWith(AndroidJUnit4.class)
+public class IdlingResourceMenuActivityTest {
+
+
+    //We begin with an ActivityTestRule on our MenuActivity.
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule =
+            new ActivityTestRule<>(MenuActivity.class);
+
+
+
+
+
+    //In order to run a test with an IdlingResource, we'll need to register the idling resource before
+    //the test is run. So we use the @Before annotation which calls out a block a code before the test runs.
+    //Sidenote: The creation of an Activity happens in onCreate, which occurs BEFORE the @Before block.
+    private IdlingResource mIdlingResource;
+    // Registers any resource that needs to be synchronized with Espresso before the test is run.
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        // To prove that the test fails, omit this call:
+        Espresso.registerIdlingResources(mIdlingResource);
+    }
+
+
+
+
+
+    @Test
+    public void idlingResourceTest() {
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
+    }
+
+
+
+
+    
+    // Remember to unregister resources when not needed to avoid malfunction.
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
+    }
+}
